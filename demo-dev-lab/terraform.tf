7c6d3e3a7cc4d8978f7c6c5164c663850d61a14f
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-10-31 21:18:09 -0400

    Modify Azure provider configuration in terraform.tf
    Updated the Azure subscription and tenant IDs in the Terraform configuration.

--- demo-dev-lab/terraform.tf
+++ demo-dev-lab/terraform.tf
@@ -16,9 +16,10 @@ terraform {
 
 provider "azurerm" {  
     features {}
-    subscription_id = "1dd93b0d-9968-4d42-8d5b-510d621c7866"
-    tenant_id       = "70af9f5c-0065-4148-8b47-3b5073308d2c"
+    subscription_id = "1ef93b9d-7268-4a12-8d5b-510d621cbfe1"
+    tenant_id       = "30af7de5-0a65-9348-8fc2-3b5079808d2d"
     resource_provider_registrations = "core"  
     resource_providers_to_register  = ["Microsoft.OperationalInsights"]  
     storage_use_azuread             = true
+
 }
